161da9ac98d9db370972650e8e014c706fe0f7b5
yountaechung <40027145+yountaechung@users.noreply.github.com>
2019-10-30 15:50:54 +0900

    Update 9-create-vmss.tf

--- terraform/9-create-vmss.tf
+++ terraform/9-create-vmss.tf
@@ -78,6 +78,7 @@ resource "azurerm_virtual_machine_scale_set" "main" {
       primary                                 = true
       subnet_id                               = "${azurerm_subnet.compute.id}"
       load_balancer_backend_address_pool_ids  = ["${azurerm_lb_backend_address_pool.main.id}"]
+      load_balancer_inbound_nat_rules_ids     = ["${azurerm_lb_nat_pool.main.id}"]
     }
   }
 }
